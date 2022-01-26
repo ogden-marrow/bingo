07efd455ec7a1f6811a74c16e02ec5fca95db77f
John Palenchar <jpalenchar.3@gmail.com>
2022-01-25 20:55:44 -0500

    got the thing worknig

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-/*jshint esversion: 6 */
-import express from 'express';
+/*jshint esversion: 6 */ 
+const express = require('express');
 const app = express();
-app.use();
-
-
+app.use(express.json());
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.redirect('/Web/index.html');
+res.sendFile(__dirname + '/Web/index.html');
 });
 
-const port = process.env.PORT || 5656;
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
