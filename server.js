ffe4340d94fe935a1375cd8536d48b34f53ded59
John Palenchar <jpalenchar.3@gmail.com>
2022-01-25 20:40:09 -0500

    sutff

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.get('/', (req, res) => {
-    res.redirect('/charles');
-});
+app.get('/', (req,res) =>{
+	res.redirect('/charles')
+})
 
-app.get('/charles', (req, res) => {
-    res.render('index', data.artist[0]);
-});
-app.get('/marilyn', (req, res) => {
-    res.render('index', data.artist[1]);
-});
-app.get('/jean', (req, res) => {
-    res.render('index', data.artist[2]);
-});
+  app.get('/charles', (req, res) => {
+      res.render('index', data.artist[0])
+  })
+  app.get('/marilyn', (req, res) => {
+      res.render('index', data.artist[1])
+  })
+  app.get('/jean', (req, res) => {
+      res.render('index', data.artist[2])
+  })
 
 const port = process.env.PORT || 5656;
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+	console.log(`http://localhost:${port}`);
 });
