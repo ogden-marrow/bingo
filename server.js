6a445310a1f4b57d94692902bc8cd8724f9e14bc
John Palenchar <jpalenchar.3@gmail.com>
2022-01-25 21:28:08 -0500

    updats

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 const express = require('express');
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-res.sendFile(__dirname + '/Web/index.html');
+    res.sendFile(__dirname + '/Web/index.html');
 });
 
+app.get('/script',(req,res) => {
+    res.sendFile(__dirname + '/Web/index.js');
+});
+
+app.get('/style',(req,res) => {
+    res.sendFile(__dirname + '/Web/index.css');
+});
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
