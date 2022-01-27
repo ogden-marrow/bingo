3b46f22bb8057ea19c87054dda59ad0de7efd5d5
John Palenchar <jpalenchar.3@gmail.com>
2022-01-27 00:39:09 -0500

    Update to use es modules and add unqueness to WordsAPI

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 /*jshint esversion: 6 */
-const express = require('express');
+import express, { json } from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 const app = express();
-const fs = require("fs");
-app.use(express.json());
+import { readFileSync } from "fs";
+import fetch from 'node-fetch';
+const __filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(__filename);
+app.use(json());
 const port = process.env.PORT || 3000;
 
 app.get('/WordsAPI', (req, res) => {
     res.json(getRandomWords());
 });
 
+app.get('/WordsAPITest', (req, res) => {
+    res.json(testAPI());
+});
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Web/index.html');
+    res.sendFile(dirname + '/Web/index.html');
 });
 
 app.get('/script', (req, res) => {
-    res.sendFile(__dirname + '/Web/index.js');
+    res.sendFile(dirname + '/Web/index.js');
 });
 
 app.get('/style', (req, res) => {
-    res.sendFile(__dirname + '/Web/index.css');
+    res.sendFile(dirname + '/Web/index.css');
 });
 
 
@@ -27,11 +36,34 @@ app.listen(port, () => {
 });
 
 function getRandomWords() {
-    let inWords = fs.readFileSync('Web/caleb.txt').toString().split("\n");
-    let outWords = new Array(25).fill(0);
-    for (let i = 0; i < 25; i++) {
+    let outWordsLen = 25;
+    let inWords = readFileSync('Web/caleb.txt').toString().split("\n");
+    let outWords = new Array(outWordsLen).fill(0);
+    for (let i = 0; i < outWordsLen; i++) {
         let rand = Math.floor(Math.random() * inWords.length);
-        outWords[i] = inWords[rand];
+        if (!outWords.includes(inWords[rand])) {
+            outWords[i] = inWords[rand];
+        } else {
+            i--;
+        }
     }
     return outWords;
+}
+
+function testAPI() {
+    fetch('http://localhost:3000/WordsAPI')
+        .then(res => res.json())
+        .then(out => {
+            out.forEach(element => {
+                out.forEach(element2 => {
+                    if (element != element2) {
+                        if (element.toLowerCase() == element2.toLowerCase()) {
+                            console.log("Error: " + element + " and " + element2 + " are the same word");
+                        }
+                    }
+                });
+            });
+        }
+        )
+        .catch(err => console.log(err));
 }
